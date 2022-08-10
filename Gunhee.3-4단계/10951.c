4a2b7866d7e61fc06e17a561f7b1a5dd7179ec55
Rudiium <87561400+Rudiium@users.noreply.github.com>
2022-08-10 10:21:48 +0900

    [BOJ]A+B - 4/새싹/0s
    https://www.acmicpc.net/problem/10951

---
+++ Gunhee.3-4단계/10951.c
@@ -0,0 +1,12 @@
+#pragma warning(disable:4996)
+#include<stdio.h>
+int main()
+{
+	int num1, num2;
+	while (scanf("%d %d",&num1,&num2)!=EOF)
+	{
+		printf("%d\n", num1 + num2);
+	}
+	return 0;
+	
+}
